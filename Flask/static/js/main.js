5996dbae1bad84d684739b54f8bb2d68b9ea9dc0
Carlos Isaza <choloisaza@outlook.de>
2018-04-05 18:33:44 +0200

    Added more times to lights and co2. Added button to turn everything off

--- Flask/static/js/main.js
+++ Flask/static/js/main.js
@@ -2,8 +2,8 @@
  * @Author: Carlos Isaza <Cholox>
  * @Date:   22-Nov-2017
  * @Project: https://github.com/cholox/automarium
- * @Last modified by:   Carlos Isaza
- * @Last modified time: 18-Dec-2017
+ * @Last modified by:   isaza
+ * @Last modified time: 05-Apr-2018
  * @License: MIT
  */
 
@@ -14,18 +14,28 @@
    $('#change_light_schedule').click(function(event) {
      var light_time1 = $('#light_time1').val();
      var light_time2 = $('#light_time2').val();
-     var data = {time1: light_time1, time2: light_time2};
+     var light_time3 = $('#light_time3').val();
+     var light_time4 = $('#light_time4').val();
+     var data = {time1: light_time1, time2: light_time2, time3: light_time3, time4: light_time4};
      if (light_time1 != "" && light_time2 != "") {
         socket.emit('change_light_schedule', data=JSON.stringify(data));
     }
    });
+   $('#turn_off_light').click(function(event) {
+       socket.emit('turn_off_light');
+   });
    $('#change_co2_schedule').click(function(event) {
      var co2_time1 = $('#co2_time1').val();
      var co2_time2 = $('#co2_time2').val();
-     var data = {time1: co2_time1, time2: co2_time2};
+     var co2_time3 = $('#co2_time3').val();
+     var co2_time4 = $('#co2_time4').val();
+     var data = {time1: co2_time1, time2: co2_time2, time3: co2_time3, time4: co2_time4};
      if (co2_time1 != "" && co2_time2 != "") {
         socket.emit('change_co2_schedule', data=JSON.stringify(data));
      }
+   });
+   $('#turn_off_co2').click(function(event) {
+       socket.emit('turn_off_co2');
    });
    $('#change_fertilizer_schedule').click(function(event) {
      var fertilizer_time = $('#fertilizer_time').val();
@@ -34,6 +44,9 @@
      if (fertilizer_time != "" && fertilizer_amount != ""){
         socket.emit('change_fertilizer_schedule', data=JSON.stringify(data));
      }
+   });
+   $('#turn_off_fertilizer').click(function(event) {
+       socket.emit('turn_off_fertilizer');
    });
    var currentTemperature = '';
    $('#current-temperature').html(currentTemperature);
@@ -52,7 +65,9 @@
         socket.emit('change_relay2_schedule', data=JSON.stringify(data));
     }
    });
-
+   $('#turn_off_relay2').click(function(event) {
+       socket.emit('turn_off_relay2');
+   });
    socket.on('connect', function(data) {
      console.log('Connected');
    });
